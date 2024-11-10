6584a6fa23370d33eacc23afa4cd7356973f2795
ipipi2024 <ipulepipi60@gmail.com>
2024-11-09 19:25:58 -0500

    removed eslint

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -7,7 +9,7 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose 
 
 if(!cached) {
   cached = (global as any).mongoose = { 
